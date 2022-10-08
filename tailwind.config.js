48786a999a29f820bf8109bffdad08b0b572c945
twills <treav.wills@gmail.com>
2022-10-08 00:22:07 -0600

    more html setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/htmlTests/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
